93b6abd22e90f9cb83636812674b7a3e183b966a
Bhasher <github.it@bhasher.com>
2023-12-27 23:38:06 +0100

    CORS

--- backend/main.go
+++ backend/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	_ "github.com/mattn/go-sqlite3"
 
 	"git.bhasher.com/bhasher/focus/backend/db"
@@ -15,12 +16,19 @@ func main() {
 	driver := "sqlite3"
 	connStr := "db.sqlite"
 	port := "3000"
+	origins := "*"
 
 	if err := db.InitDB(driver, connStr); err != nil {
 		log.Fatal(err)
 	}
 
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: origins,
+		AllowMethods: "GET,POST,PUT,DELETE",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
 
 	app.Get("/projects", handlers.GetAllProjects)
 	app.Get("/project/:id", handlers.GetProject)
