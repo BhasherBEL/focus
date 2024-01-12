5756770b64f23a5d98b573f321caa0c0a02a61d6
Bhasher <github.it@bhasher.com>
2024-01-12 13:38:04 +0100

    Update run.sh for merged dockerfile

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+PUBLIC_BACKEND_URL=${PUBLIC_BACKEND_URL:-http://localhost:3000}
+PUBLIC_BACKEND_WSURL=${PUBLIC_BACKEND_WSURL:-${PUBLIC_BACKEND_URL/http:/ws:}}
+PUBLIC_BACKEND_WSURL=${PUBLIC_BACKEND_WSURL/https:/wss:}
+
+find frontend -type f -exec sed -i "s|http://localhost:3000|$PUBLIC_BACKEND_URL|g" {} +
+find frontend -type f -exec sed -i "s|ws://localhost:3000|$PUBLIC_BACKEND_WSURL|g" {} +
+
 cd frontend && npm run preview -- --port 4173 --host 0.0.0.0 &
 cd backend && ./main &
 
